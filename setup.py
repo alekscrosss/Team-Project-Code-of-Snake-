afa15a4a7deeb7f2fa8e3c0753a928b20d85ff49
alekscrosss <137340135+alekscrosss@users.noreply.github.com>
2023-10-17 23:15:11 +0200

    add installation instructions

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name="CoderOfSnake",
     version="0.1",
-    packages=find_packages(),
+    packages=find_namespace_packages(),
     py_modules=['helper', 'note', 'sort'],
     install_requires=[],
     entry_points={
-        'console_scripts': [
-            'coderofsnake=helper:main',
-        ],
-    }
+        'console_scripts': ['coderofsnake=helper:main',],}
 )
